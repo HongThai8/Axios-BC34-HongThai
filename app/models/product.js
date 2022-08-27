7c6e0f13138d37e54c2917725f1399a6a3386fe3
thaichh8@gmail.com <thaichh8@gmail.com>
2022-08-27 23:01:26 +0700

    first

---
+++ app/models/product.js
@@ -0,0 +1,10 @@
+function Products(_id, _taiKhoan, _hoTen, _matKhau, _email, _loaiND, _NgonNgu) {
+    this.id = _id
+    this.taiKhoan = _taiKhoan;
+    this.hoTen = _hoTen;
+    this.matKhau = _matKhau;
+    this.email = _email;
+    this.ngonNgu = _NgonNgu;
+    this.loaiND = _loaiND;
+    
+}
